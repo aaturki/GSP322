7d7d09360e2e50469ae3268b7f851892eec5254e
Turki <60894776+aaturki@users.noreply.github.com>
2022-09-26 19:31:19 +0300

    Update cloudshell.sh

--- cloudshell.sh
+++ cloudshell.sh
@@ -13,11 +13,11 @@ gcloud compute instances start bastion --zone=us-central1-b
 gcloud compute instances add-tags bastion --tags=bastion --zone=us-central1-b
 gcloud compute instances add-tags juice-shop --tags=juice-shop --zone=us-central1-b
 
-gcloud compute firewall-rules create "allow-ssh-from-iap" --network=acme-vpc --target-tags=bastion --allow=tcp:22 --source-ranges="35.235.240.0/20" --description="Narrowing SSH traffic"
+gcloud compute firewall-rules create "permit-ssh-iap-ingress-ql-160" --network=acme-vpc --target-tags=bastion --allow=tcp:22 --source-ranges="35.235.240.0/20" --description="Narrowing SSH traffic"
 
-gcloud compute firewall-rules create "allow-http-juice-shop" --network=acme-vpc --target-tags=juice-shop --allow=tcp:80 --source-ranges="0.0.0.0/0" --description="Narrowing HTTP traffic"
+gcloud compute firewall-rules create "permit-http-ingress-ql-439" --network=acme-vpc --target-tags=juice-shop --allow=tcp:80 --source-ranges="0.0.0.0/0" --description="Narrowing HTTP traffic"
 
-gcloud compute firewall-rules create "allow-ssh-mgmt-subnet" --network=acme-vpc --target-tags=juice-shop --allow=tcp:22 --source-ranges="192.168.10.0/24" --description="Narrowing SSH traffic"
+gcloud compute firewall-rules create "permit-ssh-internal-ingress-ql-144" --network=acme-vpc --target-tags=juice-shop --allow=tcp:22 --source-ranges="192.168.10.0/24" --description="Narrowing SSH traffic"
 
 # dosn't work, ssh trough cloud shell
 # gcloud beta compute ssh --zone "us-central1-b" "bastion" 
