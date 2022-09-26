31345140462841928c1e4d97a3502f3ac8132872
Turki <60894776+aaturki@users.noreply.github.com>
2022-09-26 20:44:37 +0300

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -10,8 +10,8 @@
 # done
 
 
-gcloud compute firewall-rules create "allow-ssh-from-iap" --network=acme-vpc --target-tags=bastion --allow=tcp:22 --source-ranges="35.235.240.0/20" --description="Narrowing SSH traffic"
+gcloud compute firewall-rules create "permit-ssh-iap-ingress-ql-395" --network=acme-vpc --target-tags=bastion --allow=tcp:22 --source-ranges="35.235.240.0/20" --description="Narrowing SSH traffic"
 
-gcloud compute firewall-rules create "allow-http-juice-shop" --network=acme-vpc --target-tags=juice-shop --allow=tcp:80 --source-ranges="0.0.0.0/0" --description="Narrowing HTTP traffic"
+gcloud compute firewall-rules create "permit-http-ingress-ql-373" --network=acme-vpc --target-tags=juice-shop --allow=tcp:80 --source-ranges="0.0.0.0/0" --description="Narrowing HTTP traffic"
 
-gcloud compute firewall-rules create "allow-ssh-mgmt-subnet" --network=acme-vpc --target-tags=juice-shop --allow=tcp:22 --source-ranges="192.168.10.0/24" --description="Narrowing SSH traffic"
+gcloud compute firewall-rules create "permit-ssh-internal-ingress-ql-224" --network=acme-vpc --target-tags=juice-shop --allow=tcp:22 --source-ranges="192.168.10.0/24" --description="Narrowing SSH traffic"
